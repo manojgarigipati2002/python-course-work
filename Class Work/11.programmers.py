d0a7993dde14aaa1493ca0270cf9427b975e3c6f
Manoj Garigipati <manojgarigipati2002@gmail.com>
2025-11-27 10:17:45 +0530

    added operstions on string python file

---
+++ Class Work/11.programmers.py
@@ -0,0 +1,28 @@
+num=input("enter the moblie number")
+
+
+if len(num)==10:
+    if num.isdigit():
+        sts='6789'
+        if num[0] in sts:
+            print("valid number")
+        else:
+            print("number needs to starts with 6-9")
+    else:
+       print("enter the digits propery [0-9]")
+else:
+    print("length needs to 10")
+num=input("enter the moblie number")
+
+
+if len(num)==10:
+    if num.isdigit():
+        sts='6789'
+        if num[0] in sts:
+            print("valid number")
+        else:
+            print("number needs to starts with 6-9")
+    else:
+       print("enter the digits propery [0-9]")
+else:
+    print("length needs to 10")
